04f8120ef49206bbc33d6a3cf26ccbb137a29c31
RBhalugad <randhirpatel.naukri@gmail.com>
2026-06-15 20:22:52 +0530

    feat: initialize BaseTest class with automated Chrome driver setup and CI compatibility

--- src/test/java/com/randhir/BaseTest/BaseTest.java
+++ src/test/java/com/randhir/BaseTest/BaseTest.java
@@ -16,16 +16,16 @@ public class BaseTest {
   public void setUpDriver() {
     // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
     // + "//Driver//chromedriver.exe");
-    
+
     // We can rely on Selenium 4.6.0+ built-in Selenium Manager to handle drivers automatically!
     ChromeOptions options = new ChromeOptions();
     // In GitHub Actions (CI), we typically need some extra arguments to ensure Chrome doesn't crash
     if (System.getenv("GITHUB_ACTIONS") != null) {
-        options.addArguments("--headless=new");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
+      options.addArguments("--headless=new");
+      options.addArguments("--disable-dev-shm-usage");
+      options.addArguments("--no-sandbox");
     }
-    
+
     this.driver = new ChromeDriver(options);
     this.driver.manage().window().maximize();
   }
