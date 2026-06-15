d529bb016e93a1d0ff4df9a762d7d4718f8d227d
RBhalugad <randhirpatel.naukri@gmail.com>
2026-06-15 20:16:12 +0530

    feat: add BaseTest configuration with CI support and resolve SLF4J provider warning

--- src/test/java/com/randhir/BaseTest/BaseTest.java
+++ src/test/java/com/randhir/BaseTest/BaseTest.java
@@ -1,10 +1,10 @@
 package com.randhir.BaseTest;
 
 import com.google.common.util.concurrent.Uninterruptibles;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -16,8 +16,17 @@ public class BaseTest {
   public void setUpDriver() {
     // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
     // + "//Driver//chromedriver.exe");
-    WebDriverManager.chromedriver().setup();
-    this.driver = new ChromeDriver();
+    
+    // We can rely on Selenium 4.6.0+ built-in Selenium Manager to handle drivers automatically!
+    ChromeOptions options = new ChromeOptions();
+    // In GitHub Actions (CI), we typically need some extra arguments to ensure Chrome doesn't crash
+    if (System.getenv("GITHUB_ACTIONS") != null) {
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+    }
+    
+    this.driver = new ChromeDriver(options);
     this.driver.manage().window().maximize();
   }
 
